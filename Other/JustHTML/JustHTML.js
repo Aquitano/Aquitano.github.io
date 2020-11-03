fc5302768c39e2bd2396b9c2919ac318f9e22c11
Aquitano <Aquitanobaut@web.de>
2020-11-03 12:05:59 +0100

    Added JustHTML

---
+++ Other/JustHTML/JustHTML.js
@@ -0,0 +1,30 @@
+/*!
+ * JustHTML v.1.0-alpha
+ * Copyright 2020 Aquitano.
+ * Licensed under MIT (https://github.com/Aquitano/JustHTML/blob/main/LICENSE)
+ */
+
+const appear = document.querySelectorAll(".AppearOnScroll");
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+appear.forEach((appear) => {
+  appearOnScroll.observe(appear);
+});
