e510f3bcab170de7d5691a6db5575317bec18278
Aquitano <breindlthomas.Aq+account@gmail.com>
2021-03-09 21:45:04 +0100

    Update 1.6.5
    + updates JustHTML version

--- Other/JustHTML/JustHTML.js
+++ Other/JustHTML/JustHTML.js
@@ -1,29 +1,17 @@
 /*!
- * JustHTML v.1.2-alpha
+ * JustHTML v0.2-alpha
  * Copyright 2021 Aquitano.
  * Licensed under MIT (https://github.com/Aquitano/JustHTML/blob/main/LICENSE)
  */
-const appear = document.querySelectorAll(".AppearOnScroll");
-
-const appearOptions = {
-  threshold: 0,
-  rootMargin: "0px 0px -250px 0px",
-};
-
-const appearOnScroll = new IntersectionObserver(function (
-  entries,
-  appearOnScroll
-) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      return;
-    } else {
-      entry.target.classList.add("appear");
-      appearOnScroll.unobserve(entry.target);
-    }
-  });
-},
-appearOptions);
+const appear = document.querySelectorAll(".AppearOnScroll"),
+  appearOptions = { threshold: 0, rootMargin: "0px 0px -250px 0px" },
+  appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+    entries.forEach((entry) => {
+      entry.isIntersecting &&
+        (entry.target.classList.add("appear"),
+        appearOnScroll.unobserve(entry.target));
+    });
+  }, appearOptions);
 appear.forEach((appear) => {
   appearOnScroll.observe(appear);
 });
