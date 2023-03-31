0a25ea3a4e8915257f4551bfae9268e97aa75ffb
Aquitano / Thomas Breindl <Aquitano@Protonmail.com>
2023-03-31 13:53:52 +0200

    fix: centering

--- sleepy-spectrum/astro.config.mjs
+++ sleepy-spectrum/astro.config.mjs
@@ -1,12 +1,11 @@
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import { astroImageTools } from 'astro-imagetools'
-import { defineConfig } from 'astro/config'
-
-import compress from 'astro-compress'
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+import { astroImageTools } from 'astro-imagetools';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://aquitano.github.io',
-  integrations: [tailwind(), sitemap(), compress(), astroImageTools],
-})
+    site: 'https://aquitano.github.io',
+    integrations: [tailwind(), sitemap(), compress(), astroImageTools],
+});
